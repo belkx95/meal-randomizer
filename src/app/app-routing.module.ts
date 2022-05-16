af1d0f3fa0cc485a526507ada251ce0b6825dc76
Bellkim Keith Onggon <bellkimsonggon@su.edu.ph>
2022-05-16 15:55:55 +0800

    Second commit for gh pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MealDetailsComponent } from './components/meal-details/meal-details.component';
+import { MealsListComponent } from './components/meals-list/meals-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/meals-list', pathMatch: 'full' },
+  { path: 'meals-list', component: MealsListComponent },
+  { path: 'meal-details', component: MealDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MealsListComponent } from './components/meals-list/meals-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MealDetailsComponent } from './components/meal-details/meal-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    MealsListComponent
+    MealsListComponent,
+    MealDetailsComponent
   ],
   imports: [
     BrowserModule,
